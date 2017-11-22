bbade0160349877879769aa455fdaab32d61827c
nikrb <nik_rb@yahoo.com>
2017-11-22 21:52:02 +0000

    add a link to about page in main app

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import InputPreview from "../components/InputPreview";
 import {connect} from "react-redux";
 import {setMessage} from "../actions/message";
+import {Link} from "react-router-dom";
 
 class App extends React.Component {
   onChange = (value) => {
@@ -9,7 +10,14 @@ class App extends React.Component {
   };
   render(){
     const {message} = this.props.messageReducer;
-    return (<InputPreview value={message} onChange={this.onChange} />);
+    return (
+      <div>
+        <InputPreview value={message} onChange={this.onChange} />
+        <Link to="/about">
+          <button>Go About</button>
+        </Link>
+      </div>
+    );
   }
 }
 
