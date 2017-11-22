d0dd70f4480cd211e927d28149f657a8539f606b
nikrb <nik_rb@yahoo.com>
2017-11-22 21:28:34 +0000

    add actions for input preview

--- src/components/InputPreview.js
+++ src/components/InputPreview.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default class InputPreview extends React.Component {
+  onChange = (e) => {
+    this.props.onChange( e.target.value);
+  };
   render(){
     return (
       <div>
-        <input type="text" value={this.props.value} />
+        <input type="text" value={this.props.value} onChange={this.onChange} />
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import InputPreview from "../components/InputPreview";
 import {connect} from "react-redux";
+import {setMessage} from "../actions/message";
 
 class App extends React.Component {
+  onChange = (value) => {
+    this.props.dispatch( setMessage( value));
+  };
   render(){
     const {message} = this.props.messageReducer;
-    return (<InputPreview value={message} />);
+    return (<InputPreview value={message} onChange={this.onChange} />);
   }
 }
 
